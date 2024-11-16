35e8430ee84cf8bfde9b96db0ca4af9bf8df6642
Hamunii <samuel.suni04@gmail.com>
2024-11-16 12:54:02 +0200

    add cc 100

--- init.lua
+++ init.lua
@@ -158,6 +158,7 @@ vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
+vim.opt.cc = '100'
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
